c1ee8d05656f42cb8a5b203b61f97a5bde84e45c
Admin <1033948449@qq.com>
2017-04-27 14:13:35 +0800

    3

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
-// import HtmlWebpackPlugin from  'html-webpack-plugin';
 module.exports={
     entry:'./src/components/GalleryByReactApp.js',
     output: {
@@ -10,6 +9,7 @@ module.exports={
     },
     devtool: 'eval-source-map',
     module:{
+
         loaders:[
             {
                 test:/\.css$/,
@@ -57,7 +57,8 @@ module.exports={
         new HtmlWebpackPlugin({
             template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
         }),
-        new ExtractTextPlugin("styles.css"),
+        // new ExtractTextPlugin("styles.css"),
+
         new webpack.HotModuleReplacementPlugin()//热加载插件
     ],
 
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,7 +3,7 @@
  */
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: __dirname + "/app/main.js",
@@ -34,6 +34,10 @@ module.exports = {
     ],
 
     plugins: [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.AggressiveMergingPlugin(),//优化生成的chunk,合并相同的chunk
+        new webpack.NoErrorsPlugin(),    //保证编译过程不能出错
         new HtmlWebpackPlugin({
             template: __dirname + "/app/index.tmpl.html"
         }),
