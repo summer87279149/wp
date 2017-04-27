470424e711ee07cbae343332740ff4c0de5d7f39
Admin <1033948449@qq.com>
2017-04-27 14:43:12 +0800

    加入图片

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var autoprefixer = require('autoprefixer');
 module.exports={
     entry:'./src/components/GalleryByReactApp.js',
     output: {
@@ -25,7 +26,7 @@ module.exports={
             },
             {
                 test: /\.less$/,
-                loader: 'style-loader!css-loader!autoprefixer-loader{browsers:["last 2 version"]}!less-loader'
+                loader: 'style-loader!css-loader!autoprefixer-loader!less-loader'
             },
             {
                 test: /\.(png|jpg)$/,
@@ -37,8 +38,6 @@ module.exports={
             }
         ]
     },
-
-
 
     plugins: [
         new webpack.LoaderOptionsPlugin({
@@ -57,9 +56,7 @@ module.exports={
         new HtmlWebpackPlugin({
             template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
         }),
-        // new ExtractTextPlugin("styles.css"),
-
+        new ExtractTextPlugin("styles.css"),
         new webpack.HotModuleReplacementPlugin()//热加载插件
     ],
-
 }
